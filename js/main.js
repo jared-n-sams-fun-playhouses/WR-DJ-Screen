72d8a28450a3c35066734572d35b5199df1badac
Jared Smith <jared.smith.jrod@gmail.com>
2015-10-17 19:24:30 -0700

    Changing the structure a little bit, some refactoring as well

---
+++ js/main.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', function() {
+	'use strict';
+
+	var translate = navigator.mozL10n.get;
+
+	navigator.mozL10n.once(start);
+
+	function start() {
+		var title = document.getElementById('title');
+		title.textContent = translate('app_title');
+
+		setup();
+	}
+
+	function setup() {
+		setAudio();
+	}
+
+});
+
+function setAudio() {
+	window.AudioContext = (window.AudioContext || window.webkitAudioContext);
+
+	navigator.getUserMedia =   (navigator.getUserMedia || 
+								navigator.webkitGetUserMedia ||
+								navigator.mozGetUserMedia ||
+								navigator.msGetUserMedia);
+
+	window.URL =   (window.URL ||
+					window.webkitURL ||
+					window.mozURL ||
+					window.msURL);
+
+	var audio = document.querySelector('audio');
+
+	var context = new AudioContext();
+
+	navigator.getUserMedia({audio:true}, function(stream) {
+
+		var microphone = context.createMediaStreamSource(stream);
+		var filter = context.createBiquadFilter();
+
+		microphone.connect(filter);
+		filter.connect(context.destination);
+
+		audio.src = window.URL.createObjectURL(stream);
+		
+	}, 	function (error) {
+		console.log('getUserMedia error: ' + error.code + ' .');
+	});
+}
